5a236f2f38eb10daf48d448b183d107a4807da3d
Lambert1217 <1904310303@qq.com>
2024-08-20 22:38:44 +0800

    项目搭建

---
+++ Core/include/Debug/lbLog.h
@@ -0,0 +1,60 @@
+/**
+ ***************************************************************
+ * @file            : lbLog.cpp
+ * @author          : Lambert
+ * @brief           : 基于spdlog的日志系统
+ * @attention       : None
+ * @data            : 2024/8/20
+ ***************************************************************
+ */
+//
+
+#pragma once
+
+#include "spdlog/fmt/ostr.h"
+#include "spdlog/spdlog.h"
+
+namespace Lambix
+{
+
+	class lbLog
+	{
+	public:
+		/**
+		 * @brief 日志系统初始化
+		 *
+		 */
+		static void init();
+
+		/**
+		 * @brief Get the Logger object
+		 *
+		 * @return std::shared_ptr<spdlog::logger>&
+		 */
+		inline static std::shared_ptr<spdlog::logger> &GetLogger()
+		{
+			return s_Logger;
+		}
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_Logger;
+	};
+
+} // Lambix
+
+// 日志系统输出宏
+#define LOG_TRACE(...) ::Lambix::lbLog::GetLogger()->trace(__VA_ARGS__)
+#define LOG_INFO(...) ::Lambix::lbLog::GetLogger()->info(__VA_ARGS__)
+#define LOG_WARN(...) ::Lambix::lbLog::GetLogger()->warn(__VA_ARGS__)
+#define LOG_ERROR(...) ::Lambix::lbLog::GetLogger()->error(__VA_ARGS__)
+#define LOG_FATAL(...) ::Lambix::lbLog::GetLogger()->fatal(__VA_ARGS__)
+
+// 断言
+#define LOG_ASSERT(x, ...)                                   \
+	{                                                        \
+		if (!(x))                                            \
+		{                                                    \
+			LOG_ERROR("Assertion Failed: {0}", __VA_ARGS__); \
+			__debugbreak();                                  \
+		}                                                    \
+	}
---
+++ Sandbox/main.cpp
@@ -0,0 +1,8 @@
+#include "Debug/lbLog.h"
+
+int main()
+{
+	Lambix::lbLog::init();
+	LOG_INFO("Hello Lambix Core!");
+	return 0;
+}
